fix: fix the bug of atomic energies loss/error_table calculation

mace/data/atomic_data.py
@@ -76,7 +76,7 @@ class AtomicData(torch_geometric.data.Data):
         virials: Optional[torch.Tensor],  # [1,3,3]
         dipole: Optional[torch.Tensor],  # [, 3]
         charges: Optional[torch.Tensor],  # [n_nodes, ]
-        atomic_energies: Optional[torch.Tensor],  # [n_nodes, ]
+        atomic_energies: Optional[torch.Tensor],  # [n_nodes, 1]
         polarizability: Optional[torch.Tensor],  # [1, 3, 3]
         elec_temp: Optional[torch.Tensor],  # [,]
         total_charge: Optional[torch.Tensor] = None,  # [,]
@@ -109,7 +109,7 @@ class AtomicData(torch_geometric.data.Data):
         assert virials is None or virials.shape == (1, 3, 3)
         assert dipole is None or dipole.shape[-1] == 3
         assert charges is None or charges.shape == (num_nodes,)
-        assert atomic_energies is None or atomic_energies.shape == (num_nodes,)
+        assert atomic_energies is None or atomic_energies.shape == (num_nodes, 1)
         assert elec_temp is None or len(elec_temp.shape) == 0
         assert total_charge is None or len(total_charge.shape) == 0
         assert total_spin is None or len(total_spin.shape) == 0
@@ -322,9 +322,9 @@ class AtomicData(torch_geometric.data.Data):
         atomic_energies = (
             torch.tensor(
                 config.properties.get("atomic_energies"), dtype=torch.get_default_dtype()
-            )
+            ).reshape(-1, 1)
             if config.properties.get("atomic_energies") is not None
-            else torch.zeros(num_atoms, dtype=torch.get_default_dtype())
+            else torch.zeros(num_atoms, 1, dtype=torch.get_default_dtype())
         )
         elec_temp = (
             torch.tensor(